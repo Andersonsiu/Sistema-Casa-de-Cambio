af8040da90d3e7c6b5b4d70d2bde681801c7180d
Andersonsiu <118629735+Andersonsiu@users.noreply.github.com>
2025-04-10 17:00:28 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,8 @@ from flask_pymongo import PyMongo
 from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'
-app.config['MONGO_URI'] = 'mongodb://mongo_db:27017/casa_de_cambios'  # Cambia 'localhost' a 'mongo_db'
+app.config['SECRET_KEY'] = ""
+app.config['MONGO_URI'] = ""
 
 mongo = PyMongo(app)
 login_manager = LoginManager()
